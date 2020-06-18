6e2f1db7ab208195556187fe8c5f2d3d167b1005
Shirlosh <65069217+Shirlosh@users.noreply.github.com>
2020-06-18 19:18:31 +0300

    Update ListDisplayFunctions.h

--- ListDisplayFunctions.h
+++ ListDisplayFunctions.h
@@ -1,51 +1,37 @@
-//
-// Created by Idan Hauser on 05/05/2020.
-//
 
 #ifndef FINALPROJCT_LISTDISPLAYFUNCTIONS_H
 #define FINALPROJCT_LISTDISPLAYFUNCTIONS_H
 #define _CRT_SECURE_NO_WARNINGS
-
 #include "Functions.h"
 
-
-#define StartFlag '#'
-
-///***List Defintions***
+//***List Defintions***//
 typedef struct _moveCell {
-    Move move;
-    struct _moveCell *next, *prev;
+	Move move;
+	struct _moveCell *next, *prev;
 } moveCell;
 
 typedef struct _movesList {
-    moveCell *head;
-    moveCell *tail;
+	moveCell *head;
+	moveCell *tail;
 } movesList;
 
 
+//prototypes for ex2 = prints the board with the movelist :
 int buildingGamePlay(char **gameBoard, movesList *moves_list, const boardPos start, const char **board);
-
 int display(movesList *moves_list, boardPos start, char **board);
-
 void deleteNodeFromList(movesList *moves_list, moveCell *toDelete);
-
 bool checkBoardCell(const char **board, char **gameBoard, Move pMove);
-
-
 void printGameBoard(char **pGameBoard);
+// 
 
-///***List functions***
 
+//***List functions***//
 void insertNodeToEndList(movesList *pList, moveCell *pNode);
-
 void insertDataToEndList(movesList *pList, Move mov);
-
 moveCell *CreateNode(movesList *pList, Move mov);
-
 void makeEmptyList(movesList *pList);
-
 bool isEmptyList(movesList *pList);
-
 void insertDataToHeadList(movesList *pList, Move move);
+void freeMoveList(movesList *pList);
 
 #endif //FINALPROJCT_LISTDISPLAYFUNCTIONS_H
