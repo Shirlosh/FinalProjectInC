d5407532fd6ebb7c6c67dd6a394b9189ddccafea
Shirlosh <65069217+Shirlosh@users.noreply.github.com>
2020-06-18 19:14:32 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-////VER 2.5.5
-
 #include <stdio.h>
 #include <stdio.h>
 #include <string.h>
@@ -7,37 +5,32 @@
 #include "stdlib.h"
 #include "Functions.h"
 #include "InputData.h"
-#include "SaveToFile.h"
-#include "FindFullPathList.h"
 #include "TreePath.h"
 
 
 int main() {
+	boardPos positions[] = { "A1","A2","G3","F1","E5"}; ///ex5
+	boardPosArray pos_arr = { 5,positions };///ex5
+	//boardPosArray **arrayWithValidPositions = NULL;
+	boardPos start = {'A',2};///ex4
+	movesArray **moves = NULL;///all
+	char **board = NULL;///all
+	movesList *movesList = NULL;///ex2
+	int res;
 
-    char *file_name = NULL;
-    boardPosArray **arrayWithValidPositions = NULL;
-    boardPos start="A2";
-    movesArray **pMoves_arr = NULL;
-    char **pboard = NULL;
-    movesList *pMovesList = NULL;
-    pMoves_arr = getMovesArray();
-    pboard = getCharBoard();
-    arrayWithValidPositions = validMoves(pMoves_arr, pboard);///this is the for Part 1 of the project
-    pMovesList = getMoveList();
-    display(pMovesList, start, pboard);///this is the for Part 2 of the project
+	board = getCharBoard();
+	moves = getMovesArray();
+	//movesList = getMoveList();
 
-    start[0] = 'A';
-    start[1] = (char)2;
-    findAllPossiblePaths(start, pMoves_arr, pboard);///this is the for Part 3 of the project
+	//arrayWithValidPositions = validMoves(moves, board);///ex1
+	//display(movesList, start, board);///ex2
+	//findAllPossiblePaths(start, moves, board);///ex3
+	//movesList = findPathCoveringAllBoard(start, moves, board);///ex4
+	saveListToBinFile("testBin.bin", &pos_arr);///ex5
+	res = checkAndDisplayPathFromFile("testBin.bin", moves, board);
 
-      findPathCoveringAllBoard(start,pMoves_arr,pboard);//This is for part 4////
-
- //   saveListToBinFile(file_name, arrayWithValidPositions[0]);//This is for part 5////todo : need to check why this functions calls only as one dimesntion arr,,,
-
-
-
-    freeMoveList(pMovesList);
-    freeGameBord(pboard);
-    freeMovmentArray(pMoves_arr);
+	//freeMoveList(movesList);
+	//freeGameBord(board);
+	//freeMovmentArray(moves);
 
 }
